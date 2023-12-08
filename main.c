437a4aa4fe370e98aca548b06aaf82154aca91c8
Luiz Henrique Texeira Viana <109428692+Henrique1201@users.noreply.github.com>
2023-12-08 09:41:48 -0300

    main_fixed

--- main.c
+++ main.c
@@ -2,16 +2,31 @@
 #include <stdlib.h>
 #include "lib/pgm.h"
 
-int main(int argc, char *argv[]) {
+int main()
+{
+    // Inicio da medição do tempo
+	clock_t begin, end;
+	double time_total=0;
+    begin = clock();
+    DIR *d;
+    struct dirent *dir;
+    d = opendir("./images");
+    if (d)
+    {
+        while ((dir = readdir(d)) != NULL)
+        {
+            //"." e ".." são os diretórios "atual" e "anterior" e foram
+            // removido para evitar poluição na saída
+            if(dir->d_name[0] != '.'){
+                begin = clock();
+                printf("%s\n", dir->d_name);
 
-    struct Image img;
-    struct Image new_image;
-    char *filename = argv[1];
-
-    readPGMImage(&img, filename);
-    viewPGMImage(&img);
-
-    writePGMImage(&img, "teste.pgm");
-
-    return 0;
+            }
+        }
+        closedir(d);
+    }
+    // Fim da medição do tempo  
+    end = clock();
+    time_total = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Tempo Total: %lf\n",time_total);
 }
