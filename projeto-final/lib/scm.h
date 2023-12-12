1b2daf815abaac9830f624f4152ed8a6ea04d607
Luiz Henrique Texeira Viana <109428692+Henrique1201@users.noreply.github.com>
2023-12-11 23:08:23 -0300

    Delete projeto-final directory

--- projeto-final/lib/scm.h
+++
@@ -1,4 +0,0 @@
-#include "../lib/pgm.h"
-
-void applyscm(char *imagemEntrada, char *imagemSuavizada, int scmNivel);
-//int applyscm(char *imagemEntrada, char *imagemSuavizada, char *arquivoSaida, int scmNivel);
